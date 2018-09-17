1a7a6a8fb0c34dc03e28d381a5e4d1ff105d058d
Leszek Macura <leszek@macura.co>
2018-09-17 20:42:41 +0200

    Finish carusel 10.1

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
+
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
+
+var flkty = new Flickity(elem, {
+  // options  
   autoPlay: 2000,
   freeScroll: true,
-  pageDots: false, 
+  pageDots: false,
   cellAlign: 'left',
-  contain: true  
+  contain: true,
+  hash: true,
 });
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-  
-});
-
-var flkty = new Flickity('.carousel');
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
-buttonGroup.addEventListener( 'click', function( event ) {
+buttons = fizzyUIUtils.makeArray(buttons);
+buttonGroup.addEventListener('click', function (event) {
   // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
+  if (!matchesSelector(event.target, '.button')) {
     return;
   }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
+  var index = buttons.indexOf(event.target);
+  flkty.select(index);
 });
 
 
