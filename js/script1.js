f30f33b03ecb0a63a71e3706b8f9fb03d200b152
Leszek Macura <leszek@macura.co>
2018-09-22 12:14:51 +0200

    Finish Mustache - Zad 10_2

--- js/script1.js
+++ js/script1.js
@@ -1,58 +1,28 @@
 'use strict';
-(function(){ 
+(function () {
+
 	/* W kodzie HTML tego przykładu  dodaliśmy też dwa szablony: 
-	- template-product-list, który jest wrapperem listy produktów, oraz
-	- template-product-item, który jest szablonem pojedynczej pozycji na liście. 
-	
-	Oprócz tego dodaliśmy też tablicę, zapisaną w zmiennej productsData, zawierającą kilka obiektów. Każdy z nich reprezentuje jeden produkt, który ma podaną nazwę oraz cenę. 
-	
-	Zaczniemy od stworzenia zmiennych z kodem naszych szablonów
+	- template-product-list, który jest wrapperem caruzeli , oraz
+	- template-product-item, który jest szablonem pojedynczej lini okrteślającej pojedynczy slajd. 	
+	Oprócz tego dodaliśmy też tablicę, zapisaną w zmiennej productsData, zawierającą adres do obrazka. 	
+	Szablony:
 	*/
-	
 	var templateList = document.getElementById('template-product-list').innerHTML;
 	var templateItem = document.getElementById('template-product-item').innerHTML;
-	
-	// Następnie zoptymalizujemy drugą z nich, ponieważ tylko ona będzie wykonywana wielokrotnie. 	
 
+	// Następnie zoptymalizujemy drugą z nich, ponieważ tylko ona będzie wykonywana wielokrotnie. 	
 	Mustache.parse(templateItem);
-	
-	// Teraz stworzymy zmienną, w której chcemy mieć kod HTML wszystkich produktów. 
-	
+	// Mustache.parse(fullProductList); // jednak ta linijka nie była potrzebna :-)
+
+	// kod HTML wszystkich slajdów. 	
 	var listItems = '';
-	
-	/* Czas napisać pętlę, która dla każdego elementu z listy:
-	1. wygeneruje kod HTML dla danego produktu, oraz
-	2. doda ten wygenrowany kod HTML do zmiennej listItems. 
-	
-	Uwaga - zmienna productsData, której używamy poniżej, została zdefiniowana w kodzie HTML!
-	*/
-	
-	for(var i = 0; i < productsData.length; i++){
+
+	// Czas napisać pętlę, która dla każdego elementu z listy	wygeneruje kod HTML 	
+	for (var i = 0; i < productsData.length; i++) {
 		console.log(productsData);
 		listItems += Mustache.render(templateItem, productsData[i]);
 	}
-	
-	// Po wykonaniu pętli, zmienna listItems zawiera już kod HTML dla wszystkich produktów. Teraz wykorzystamy szablon templateList, aby wstawić produkty do wrappera listy. 
-	
-	var fullProductList = Mustache.render(templateList, {products: listItems});
-	
-	// I w pełni wyrenderowaną listę wyświetlimy na stronie: 
-	
+	var fullProductList = Mustache.render(templateList, { products: listItems });
 	results.insertAdjacentHTML('beforeend', fullProductList);
-	
-	/* Przeanalizuj powyższy przykład, a w szczególności zwróć uwagę na te kwestie: 
-	
-	1. Zmienna listItems po wykonaniu pętli zawiera w sobie tekst w postaci "<li>...</li><li>...</li><li>...</li>". 
-	
-	2. Ten tekst wstawiamy w kolejnym szablonie, tj. templateList, który dodaje nagłówek oraz "<ul></ul>". 
-	
-	3. W kodzie szablonu listy (template-product-list) nazwa "products" jest zamknięta w potrójnych nawiasach klamrowych, zamiast podwójnych. Jak możesz sprawdzić w dokumentacji pluginu Mustache.js, dzięki temu zawartość "zmiennej" products została wstawiona jako kod HTML, a nie jako tekst. Spróbuj zmienić potrójny nawias klamrowy na podwójny i zobacz jak zmieni się wynik wyświetlony na stronie. 
-	
-	ĆWICZENIE:
-	1. Do każdego obiektu w tablicy productsData (która znajduje się w kodzie HTML) dodaj właściwość o nazwie "image" i wartości w postaci linka do jakiegoś obrazka, np. image: "http://via.placeholder.com/200x100/ffaaaa/000000?text=basketball".
-	2. Zmień szablon template-product-item w taki sposób, aby nad tekstem wyświetlał się obrazek, którego adres jest podany w polu "image" tego produktu. 
-	
-	Zwróć uwagę, że w tym ćwiczeniu w ogóle nie musisz zmieniać kodu JS!
-	*/
-	
+
 })(); 
